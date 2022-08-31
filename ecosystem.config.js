5ee37457391325a2132ee74c9fbad7f73a3be704
wearenv-digital <design@wearenv.co.uk>
2022-08-31 14:05:26 +0100

    path names

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,17 +1,17 @@
 module.exports = {
-  apps : [
-      {
-        name: "myserver",
-        script: "server.js",
-        watch: true,
-        env: {
-            "PORT": 3000,
-            "NODE_ENV": "development"
-        },
-        env_production: {
-            "PORT": 8080,
-            "NODE_ENV": "production",
-        }
-      }
-  ]
-}
+	apps: [
+		{
+			name: "myserver",
+			script: "/server.js",
+			watch: true,
+			env: {
+				"PORT": 3000,
+				"NODE_ENV": "development",
+			},
+			env_production: {
+				"PORT": 8080,
+				"NODE_ENV": "production",
+			},
+		},
+	],
+};
