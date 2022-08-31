aacc3a2de15243425cca20023b274fc6a6e85751
wearenv-digital <design@wearenv.co.uk>
2022-08-31 15:15:35 +0100

    route handling

--- server.js
+++ server.js
@@ -6,10 +6,10 @@ const nodemailer = require("nodemailer");
 
 const PORT = process.env.PORT || 5500; // CHANGE TO WHATEVER
 // middleware
-// var htmlPath = path.join(__dirname, "html");
-// app.use(express.static("public"));
-// app.use(express.static(__dirname));
-app.use(express.static(path.join(__dirname, "html")));
+var htmlPath = path.join(__dirname, "html/");
+app.use(express.static("html"));
+app.use(express.static(__dirname));
+app.use(express.static(path.join(__dirname, "/html")));
 // app.use(express.static(htmlPath));
 app.use(express.json());
 
@@ -17,10 +17,80 @@ app.use(express.json());
 // 	// res.send("Hello");
 // 	res.send(__dirname + "/html");
 // });
+// var entirePath =
+app.get("/", (req, res) => {
+	res.sendFile(htmlPath + "/index.html");
+});
+
+app.get("/about", (req, res) => {
+	res.sendFile(htmlPath + "/about.html");
+});
+
+app.get("/contact", (req, res) => {
+	res.sendFile(htmlPath + "/contact.html");
+});
+
+app.get("/cost-reduction-managment-better", (req, res) => {
+	res.sendFile(htmlPath + "/cost-reduction-managment-better.html");
+});
+
+app.get("/credit-checking-better", (req, res) => {
+	res.sendFile(htmlPath + "/credit-checking-better.html");
+});
+
+app.get("/credit-control-tips", (req, res) => {
+	res.sendFile(htmlPath + "/credit-control-tips.html");
+});
+
+app.get("/credit-management-health-checks", (req, res) => {
+	res.send(htmlPath + "/credit-management-health-checks.html");
+});
+
+app.get("/debt-recovery-collcection-better", (req, res) => {
+	res.sendFile(htmlPath + "/debt-recovery-collection-better.html");
+});
+
+app.get("/late-paymemt-calculator", (req, res) => {
+	res.sendFile(htmlPath + "late-paymemt-calculator.html");
+});
+
+app.get("/late-payment-letter-templates", (req, res) => {
+	res.sendFile(htmlPath + "/late-payment-letter-templates.html");
+});
+
+app.get("/letter-before-action-checklist", (req, res) => {
+	res.sendFile(htmlPath + "/letter-before-action-checklist.html");
+});
+
+app.get("/links", (req, res) => {
+	res.sendFile(htmlPath + "/links.html");
+});
+
+app.get("/outsourced-credit-control", (req, res) => {
+	res.sendFile(htmlPath + "/outsourced-credit-control.html");
+});
+
+app.get("/privacy-policy", (req, res) => {
+	res.sendFile(htmlPath + "/privacy-policy.html");
+});
+
+app.get("/process-improvement", (req, res) => {
+	res.sendFile(htmlPath + "/process-improvement.html");
+});
+
+app.get("/resources", (req, res) => {
+	res.sendFile(htmlPath + "/resources.html");
+});
+
+app.get("/terms-and-conditions", (req, res) => {
+	res.sendFile(htmlPath + "/terms-and-conditions.html");
+});
+
+app.get("/training-and-consultancy-better", (req, res) => {
+	res.sendFile(htmlPath + "/training-and-consultancy-better.html");
+});
 
 app.post("/", (req, res) => {
-	console.log(req.body);
-
 	const transporter = nodemailer.createTransport({
 		service: "smtp.ionos.co.uk",
 		host: "smtp.ionos.co.uk",
@@ -49,7 +119,7 @@ app.post("/", (req, res) => {
 			console.log(err.stack);
 			res.send("error");
 		} else {
-			console.log("Email sent" + info.response);
+			console.log("Email sent" + info.res);
 			res.send("Success");
 		}
 	});
