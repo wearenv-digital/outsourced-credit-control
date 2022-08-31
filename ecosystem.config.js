d77b08e896a8e86c90ef953ff5e0fa1b4f48fdf1
wearenv-digital <design@wearenv.co.uk>
2022-08-31 10:07:08 +0000

    ecosystem-file

---
+++ ecosystem.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  apps : [
+      {
+        name: "myserver",
+        script: "server.js",
+        watch: true,
+        env: {
+            "PORT": 3000,
+            "NODE_ENV": "development"
+        },
+        env_production: {
+            "PORT": 8080,
+            "NODE_ENV": "production",
+        }
+      }
+  ]
+}
